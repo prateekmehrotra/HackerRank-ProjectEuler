b9da7faaa8e2a372c3fb510fffa847ef914d8b30
Prateek Mehrotra <mehrotra.prtk@gmail.com>
2015-07-09 15:18:07 -0700

    Project Euler+: Problem #3
    Problem Statement
    
    This problem is a programming version of Problem 3 from projecteuler.net
    
    The prime factors of 13195 are 5, 7, 13 and 29.
    
    What is the largest prime factor of a given number N?
    
    Input Format
    First line contains T, the number of test cases. This is followed by T
    lines each containing an integer N.
    
    Output Format
    For each test case, display the largest prime factor of N.
    
    Constraints
    1≤T≤10
    10≤N≤1012
    
    Sample Input
    
    2
    10
    17
    
    Sample Output
    
    5
    17

--- LargestPrimeFactor.java
+++ LargestPrimeFactor.java
@@ -1,13 +1,46 @@
 package project.euler.plus;
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class LargestPrimeFactor {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int numberOfTestCases = in.nextInt();
+        for(int i=0;i<numberOfTestCases;i++)
+            {
+            Long n = in.nextLong();
+            Long maxPrimeFactor = primeFactorize(n);
+            System.out.println(maxPrimeFactor);
+        }
+    }
+    public static Long primeFactorize(Long n)
+        {
+        try
+            {
+            Long maxPrimeFactor = 1L;
+            for(Long i=2L;i<=n/i;i++)
+                {
+                while(n%i==0)
+                    {
+                    if(i>maxPrimeFactor) maxPrimeFactor=i;
+                    n/=i;
+                }
+            }
+            if(n>1)
+                {
+                if(n>maxPrimeFactor) maxPrimeFactor = n;
+            }
+            return maxPrimeFactor;
+        }
+        catch(Exception e)
+            {
+            throw e;
+        }
+    }
 }
