520db739d1860dbfdc0db39a026b7b08d5996e02
Prateek Mehrotra <mehrotra.prtk@gmail.com>
2015-09-08 16:09:28 -0700

    Project Euler+: Problem #46
    Problem Statement
    
    This problem is a programming version of Problem 46 from
    projecteuler.net
    
    It was proposed by Christian Goldbach that every odd composite number
    can be written as the sum of a prime and twice a square.
    91521252733=7+2×12=7+2×22=3+2×32=7+2×32=19+2×22=31+2×12
    
    It turns out that the conjecture was false as you'll discover some
    values can't be represented as a sum of prime and twice a square.
    You are given N, print the number of ways N can be represented as a sum
    of prime and twice a square.
    Example 15 can be represented in two ways as 15=7+2×22 and 15=13+2×12
    
    Input Format
    The first line contains an integer T , i.e., number of test cases.
    Next T lines will contain an integer N.
    
    Output Format
    Print the values corresponding to each test case.
    
    Constraints
    1≤T≤100
    9≤N<5×105
    N∈{odd composite number}
    
    Sample Input
    
    2
    9
    15
    
    Sample Output
    
    1
    2

---
+++ GoldbachsOtherConjecture.java
@@ -0,0 +1,76 @@
+package project.euler.plus;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class GoldbachsOtherConjecture {
+    private static boolean[] prime = null;
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int numberOfTestCases = in.nextInt();
+        generatePrimes();
+        
+        for(int i=0;i<numberOfTestCases;i++)
+            {
+            int res=0;
+            int n = in.nextInt();
+            for(int j=2;j<=n;j++)
+                {
+                if(prime[j])
+                    {
+                    int diff=n-j;
+                    if(diff%2==0 && isPerfectSquare(diff/2))
+                        res+=1;
+                }
+            }
+            System.out.println(res);
+        }
+    }
+     public static boolean isPerfectSquare(int input)
+        {
+        int root = (int) Math.sqrt(input);
+        return input == root * root;
+    }
+    public static void generatePrimes()
+        {
+        int limit = 1000000;
+        prime = new boolean[limit+1];
+        prime[2] = true;
+        prime[3] = true;
+        int root = (int) Math.ceil(Math.sqrt(limit));
+        
+        //Sieve of Atkin for prime number generation
+        for (int x = 1; x < root; x++)
+            {
+            for (int y = 1; y < root; y++)
+                {
+                int n = 4 * x * x + y * y;
+                if (n <= limit && (n % 12 == 1 || n % 12 == 5))
+                    prime[n] = !prime[n];
+
+                n = 3 * x * x + y * y;
+                if (n <= limit && n % 12 == 7)
+                    prime[n] = !prime[n];
+
+                n = 3 * x * x - y * y;
+                if ((x > y) && (n <= limit) && (n % 12 == 11))
+                    prime[n] = !prime[n];
+            }
+        }
+        
+        for (int i = 5; i <= root; i++)
+            {
+            if (prime[i])
+                {
+                for (int j = i * i; j < limit; j += i * i)
+                    {
+                    prime[j] = false;
+                }
+            }
+        }
+    }
+}
